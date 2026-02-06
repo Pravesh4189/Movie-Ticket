13dc78167caecc9c616ec69da201d4e0678cafba
Pravesh Lodha <lodhapravesh0@gmail.com>
2026-02-06 10:30:41 +0530

    update

--- server/controllers/stripeWebhooks.js
+++ server/controllers/stripeWebhooks.js
@@ -15,14 +15,18 @@ export const stripeWebhooks = async (request, response)=>{
 
     try {
         switch (event.type) {
-            case "payment_intent.succeeded": {
-                const paymentIntent = event.data.object;
-                const sessionList = await stripeInstance.checkout.sessions.list({
-                    payment_intent: paymentIntent.id
-                })
-                const session = sessionList.data[0];
-                const {bookingId} = session.metadata;
-                await Booking.findByIdAndUpdate(bookingId, {
+            case "checkout.session.completed": {
+            const session = event.data.object;
+            const { bookingId } = session.metadata;
+            // case "payment_intent.succeeded": {
+            //     const paymentIntent = event.data.object;
+            //     const sessionList = await stripeInstance.checkout.sessions.list({
+            //         payment_intent: paymentIntent.id
+            //     })
+            //     const session = sessionList.data[0];
+            //     const {bookingId} = session.metadata;
+              if (bookingId) {
+                  await Booking.findByIdAndUpdate(bookingId, {
                     isPaid: true,
                     paymentLink: ""
                 })
@@ -31,7 +35,8 @@ export const stripeWebhooks = async (request, response)=>{
                 await inngest.send({
                     name:"app/show.booked",
                     data: {bookingId}
-                })  
+                });
+            } 
                 break;
             }
             default:
