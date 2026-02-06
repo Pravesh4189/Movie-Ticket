81509d97f40e9c2b4f70f3ea42a414835779b80e
Pravesh Lodha <lodhapravesh0@gmail.com>
2026-02-06 11:28:03 +0530

    update

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -100,12 +100,12 @@ const sendBookingConfirmationEmail = inngest.createFunction(
         await sendEmail({
             to:booking.user.email,
             subject:`Payment Confirmation:"${booking.show.movie.title}" booked!`,
-            body:`<div style="font-family:Arail,sans-serif;line-height:1.5;">
+            body:`<div style="font-family:Arial,sans-serif;line-height:1.5;">
             <h2>Hi ${booking.user.name},</h2>
             <p>Your booking for <strong style="color:#F84565;">"${booking.show.movie.title}"</strong> is confirmed.</p>
             <p>
             <strong>Date:</strong> ${new Date(booking.show.showDateTime).toLocaleDateString('en-US',{timeZone:'Asia/Kolkata'})}<br/>
-            <strong>Time:</strong> ${new Date(booking.show.showDateTime).toLocaleTimeString('en-US',{timeZOne:'Asia/Kolkata'})}
+            <strong>Time:</strong> ${new Date(booking.show.showDateTime).toLocaleTimeString('en-US',{timeZone:'Asia/Kolkata'})}
             </p>
             <p>Enjoy the show!</p>
             <p>Thanks for booking with us!<br/>- CineBooker Team</p>
